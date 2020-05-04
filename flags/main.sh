6762de95d4c4a14c0dfbe8290fd2d9e4f469fb36
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2020-05-05 04:40:40 +0500

    first commit

---
+++ flags/main.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Please ensure that the dependencies mentioned in the
+# readme.md file are installed on your machine before
+# executing this script.
+#     ______
+#     / __/ /___ _____ ______
+#    / /_/ / __ `/ __ `/ ___/
+#   / __/ / /_/ / /_/ (__  )
+#  /_/ /_/\__,_/\__, /____ /
+#              /____/
+#
+# author - atifcppprogrammer
+
+# Checking if exactly one argument given throwing
+# error message and exiting otherwise.
+if [ ! $# -eq 1 ]; then
+  echo "Provide alpha code of country of interest !!!";
+  exit 1;
+fi
+
+# Determining denomynm for country of interest, exiting
+# in case none found.
+url='https://restcountries.eu/rest/v2/alpha/';
+$(curl $url$1 -so .data.json)
+if [ "$(jq .status .data.json )" == "404" ]; then
+  echo "No country found against provided code !!!";
+  exit 1;
+fi
+demonym=$(jq .demonym .data.json | tr -d [=\"=]);
+demonym=${demonym,,}; rm .data.json
+
+# Loading country flag using collected demonyn and
+# printing it out in pretty manner.
+core='https://flaglane.com/download/'
+path=$demonym'-flag/'$demonym'-flag-large.jpg'
+flag=($(jp2a --color $core$path))
+for row in ${flag[@]}; do
+  echo $row; sleep 0.08
+done
---
+++ flags/readme.md
@@ -0,0 +1,17 @@
+# flags
+Uses ascii characters to print on the terminal the flag of  
+the country whose alpha code is provided as argument to  
+script, in a **cool** way.
+
+## Use
+The following examples illustrate printing the italian flag  
+using alpha2 and alpha3 codes respectively.
+- bash main.sh it
+- bash main.sh ita
+
+## Dependencies
+Please ensure that the following programs are installed on  
+your machine prior to running main.sh.
+- [jp2a](https://github.com/cslarsen/jp2a)
+- [curl](https://github.com/curl/curl)
+- [jq](https://github.com/stedolan/jq)
